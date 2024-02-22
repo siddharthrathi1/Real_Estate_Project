ced204a50fe8e383ce64d1002cc83c48e37b8b14
Siddharth Rathi <f20190730@goa.bits-pilani.ac.in>
2024-02-22 20:43:23 +0530

    Created User Model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 // const express = require('express'); This works fine as well but if u have to use the import method, we need to make some changes in package.json, by adding type: module under main
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('connected to MongoDB!');
+})
+.catch((err)=>{
+    console.log(err);
+});
 const app=express(); //creating an instance of express
 
 app.listen(3000,()=>{
